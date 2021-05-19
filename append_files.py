28ac7970e242bee24620d41c937ab9e0ba0cdd2a
greg <gregorycusack@gmail.com>
2021-05-18 21:44:23 -0600

    updates

--- append_files.py
+++ append_files.py
@@ -1,14 +1,22 @@
 import os
 from itertools import islice
-
+import matplotlib.pyplot as plt
+import matplotlib.markers as mrk
+import numpy as np
 
 def get_absolute_slack(limit, usage):
     return limit - usage
 
 
 def get_relative_slack(limit, usage):
+    if limit == 0:
+        print("limit is 0!")
+        return -1
     return (limit - usage) / limit
 
+def ap_get_usage_limit(line):
+    stats = line.rstrip().split("\t")
+    return float(stats[0]), float(stats[1])
 
 def aggregate_1s(lines):
     limit_sum = usage_sum = 0
@@ -23,10 +31,18 @@ def aggregate_1s(lines):
     return round(limit_avg, 2), round(usage_avg, 2)
 
 
-def get_slacks_from_agg_line(line):
-    stats = line.rstrip().split(",")
-    limit = float(stats[0])
-    usage = float(stats[1])
+def get_slacks_from_line(line, separator):
+    stats = line.rstrip().split(separator)
+    if separator == "\t":    #ap -> usage, limit
+        limit = float(stats[1])
+        usage = float(stats[0])
+    else: #dc               #dc -> limit, usage
+        limit = float(stats[0])
+        usage = float(stats[1])
+
+    # limit = float(stats[0])
+    # usage = float(stats[1])
+
     abs_slack = get_absolute_slack(limit, usage)
     if abs_slack < 0:
         print("[ERROR]: abs slack < 0")
@@ -38,73 +54,177 @@ def get_slacks_from_agg_line(line):
     return abs_slack, rel_slack
 
 
+def return_max_val(prev_max, new_val):
+    if new_val > prev_max:
+        return new_val
+    return prev_max
+
+
 class ManageStatistics:
-    def __init__(self, measurement_, resource_, ratio_, duration_, system_):
+    def __init__(self, measurement_, resource_, load_type):
         self.measurement = measurement_
         self.resource = resource_
-        self.ratio = ratio_
-        self.duration = duration_
-        self.system = system_
+        self.load_type = load_type
+        self.prefix_dc = "/home/greg/Desktop/CDFGenerator/data/" + self.measurement + "/" + self.load_type + "/" + \
+                      self.resource + "/dc/"
+
+        self.prefix_ap = "/home/greg/Desktop/CDFGenerator/data/" + self.measurement + "/" + self.load_type + "/" + \
+                         self.resource + "/ap/"
+        
+        self.abs_slack_file = "absolute-slacks.txt"
+        self.rel_slack_file = "relative-slacks.txt"
 
     def aggregate_per_period_to_per_second(self):
-        prefix = "/home/greg/Desktop/CDFGenerator/data/"
-        infolder = prefix + self.measurement + "/logs-" + self.resource + "-" + self.ratio + "x-" + self.duration + \
-                   "min" + "-" + self.system + "/"
+        infolder = self.prefix_dc + "raw/"
         outfolder = infolder[:-1] + "-agg/"
         if not os.path.exists(outfolder):
             os.makedirs(outfolder)
 
+        num_files_to_delete_low_usage = 0
         directory = os.fsencode(infolder)
         for file in os.listdir(directory):
             infile = os.fsdecode(file)
             infile_full_path = infolder + infile
             if infile_full_path.endswith(".txt"):
                 outfile = outfolder + infile[:-4] + "-agg-1s.txt"
+                max_usage = 0
                 with open(outfile, "w+") as outf:
                     outf.write("1S LIMIT AGG (ns), 1S USAGE AGG (ns)\n")
                     with open(infile_full_path, "r") as inf:
                         while True:
-                            lines_10 = list(islice(inf, 10))
+                            lines_10 = list(islice(inf, 1))
                             if not lines_10:
                                 break
                             limit_agg, usage_agg = aggregate_1s(lines_10)
                             outf.write(str(limit_agg) + "," + str(usage_agg) + "\n")
+                            max_usage = return_max_val(max_usage, usage_agg)
+                print("max_usage of file: " + infile + " is: " + str(max_usage))
+                if self.resource == "cpu" and max_usage < 30000000:
+                    print("max usage of container is less than 30% of a core! need to remove")
+                    os.remove(outfile)
+                    num_files_to_delete_low_usage += 1
 
-        return outfolder
+        print("number of files deleted due to old usage: " + str(num_files_to_delete_low_usage))
 
-    def aggregate_into_one_file(self, agg_file_folder):
-        prefix = "/home/greg/Desktop/CDFGenerator/data/"
-        outfolder = prefix + self.measurement + "/"
-        outfile_abs_slack = outfolder + "logs-" + self.resource + "-" + self.ratio + "x-" + self.duration + \
-                            "min" + "-" + self.system + "-ALL-absolute.txt"
-        outfile_rel_slack = outfolder + "logs-" + self.resource + "-" + self.ratio + "x-" + self.duration + \
-                            "min" + "-" + self.system + "-ALL-relative.txt"
-        directory = os.fsencode(agg_file_folder)
+    def aggregate_into_one_file(self):
+        infolder = self.prefix_dc + "raw-agg/"
+        outfolder = self.prefix_dc
+        outfile_abs_slack = outfolder + "absolute-slacks.txt"
+        outfile_rel_slack = outfolder + "relative-slacks.txt"
+        directory = os.fsencode(infolder)
         with open(outfile_abs_slack, "w+") as absf, open(outfile_rel_slack, "w+") as relf:
             for file in os.listdir(directory):
                 infile = os.fsdecode(file)
-                infile_full_path = agg_file_folder + infile
+                infile_full_path = infolder + infile
                 if infile_full_path.endswith(".txt"):
                     with open(infile_full_path, "r") as aggf:
+                        # for _ in range(15):
                         next(aggf)  # skip first line (header)
                         for line in aggf:
-                            abs_slack, rel_slack = get_slacks_from_agg_line(line)
+                            abs_slack, rel_slack = get_slacks_from_line(line, ",")
                             absf.write(str(abs_slack) + "\n")
                             relf.write(str(rel_slack) + "\n")
 
-    #
-    # filename_prefix = infolder + type + "-" + ratio + "x-v"
-    #
-    # for i in range(num_files):
-    #     infile = filename_prefix + str(i) + ".txt"
-    #     print(infile)
-    #     temp_files.append(infile)
-    #
-    # outfile = filename_prefix[:-1] + "ALL.txt"
-    # with open(outfile, "w+") as f:
-    #     for tempfile in temp_files:
-    #         with open(tempfile, "r") as tf:
-    #             for line in tf:
-    #                 f.write(line)
-    #
-    # return folder_prefix
+    def remove_low_usage_containers(self):
+        infolder = self.prefix_ap + "raw/"
+        outfolder = infolder[:-1] + "-trim/"
+        if not os.path.exists(outfolder):
+            os.makedirs(outfolder)
+
+        num_files_to_delete_low_usage = 0
+        directory = os.fsencode(infolder)
+        for file in os.listdir(directory):
+            infile = os.fsdecode(file)
+            infile_full_path = infolder + infile
+            if infile_full_path.endswith(".txt"):
+                outfile = outfolder + infile
+                max_usage = 0
+                with open(outfile, "w+") as outf:
+                    with open(infile_full_path, "r") as inf:
+                        print('reading in: ' + infile_full_path)
+                        for line in inf:
+                            usage, limit = ap_get_usage_limit(line)
+                            outf.write(str(usage) + "\t" + str(limit) + "\n")
+                            max_usage = return_max_val(max_usage, usage)
+                print("max_usage of file: " + infile + " is: " + str(max_usage))
+                if self.resource == "cpu" and max_usage < 30000: # 30000us = 30ms = 30% of core
+                    print("max usage of container is less than 30% of a core! need to remove")
+                    os.remove(outfile)
+                    num_files_to_delete_low_usage += 1
+
+        print("number of files deleted due to low usage (ap): " + str(num_files_to_delete_low_usage))
+
+    def aggregate_into_one_file_autopilot(self):
+        infolder = self.prefix_ap + "raw-trim/"
+        outfolder = self.prefix_ap
+        outfile_abs_slack = outfolder + "absolute-slacks.txt"
+        outfile_rel_slack = outfolder + "relative-slacks.txt"
+        directory = os.fsencode(infolder)
+        with open(outfile_abs_slack, "w+") as absf, open(outfile_rel_slack, "w+") as relf:
+            for file in os.listdir(directory):
+                infile = os.fsdecode(file)
+                infile_full_path = infolder + infile
+                if infile_full_path.endswith(".txt"):
+                    with open(infile_full_path, "r") as aggf:
+                        for line in aggf:
+                            abs_slack, rel_slack = get_slacks_from_line(line, "\t")
+                            if abs_slack >= 0:# and abs_slack < 50000:
+                                absf.write(str(abs_slack) + "\n")
+                            if rel_slack >= 0:
+                                relf.write(str(rel_slack) + "\n")
+
+    def run(self):
+
+        """ DC ABSOLUTE """
+        dc_absolute_slack_path = self.prefix_dc + self.abs_slack_file
+        dc_absolute_slack = np.loadtxt(dc_absolute_slack_path)
+        data_sorted_dc_absolute = np.sort(dc_absolute_slack) / 1000 / 1000 / 100  # ns -> us -> ms -> cores
+        p_dc_absolute = 1. * np.arange(len(dc_absolute_slack)) / (len(dc_absolute_slack) - 1)
+
+        """ DC RELATIVE """
+        dc_relative_slack_path = self.prefix_dc + self.rel_slack_file
+        dc_relative_slack = np.loadtxt(dc_relative_slack_path)
+        data_sorted_dc_relative = np.sort(dc_relative_slack)  # / 1000 #convert to ms
+        p_dc_relative = 1. * np.arange(len(dc_relative_slack)) / (len(dc_relative_slack) - 1)
+
+        """ AP ABSOLUTE """
+        ap_absolute_slack_path = self.prefix_ap + self.abs_slack_file
+        ap_absolute_slack = np.loadtxt(ap_absolute_slack_path)
+        data_sorted_ap_absolute = np.sort(ap_absolute_slack) / 1000 / 100  # us -> ms -> cores
+        p_ap_absolute = 1. * np.arange(len(ap_absolute_slack)) / (len(ap_absolute_slack) - 1)
+
+        """ AP RELATIVE """
+        ap_relative_slack_path = self.prefix_ap + self.rel_slack_file
+        ap_relative_slack = np.loadtxt(ap_relative_slack_path)
+        data_sorted_ap_relative = np.sort(ap_relative_slack)  # / 1000 #convert to ms
+        p_ap_relative = 1. * np.arange(len(ap_relative_slack)) / (len(ap_relative_slack) - 1)
+
+
+        fig = plt.figure()
+        # fig, axs = plt.subplot(1, 2)
+        # axs[0,0]
+
+        ax1 = fig.add_subplot(211)
+        ax1.plot(data_sorted_dc_absolute, p_dc_absolute, label="DC", marker='+', markevery=20)
+        ax1.plot(data_sorted_ap_absolute, p_ap_absolute, label="AP", marker='x', markevery=20)
+        # ax1.plot(data_ml_exact_abs_slack, p_ml_exact_abs_slack, label="ML Ideal", marker='*', markevery=20)
+        # ax1.plot(data_ml_conserv_abs_slack, p_ml_conserv_abs_slack, label="ML Conserv.", marker=mrk.TICKRIGHT, markevery=20)
+
+        ax1.set_xlabel('Absolute Slack')
+        ax1.set_ylabel('')
+
+        ax2 = fig.add_subplot(212)
+        ax2.plot(data_sorted_dc_relative, p_dc_relative, label="DC", marker='+', markevery=20)
+        ax2.plot(data_sorted_ap_relative, p_ap_relative, label="Static", marker='x', markevery=20)
+        # ax2.plot(data_ml_exact_relative_slack, p_ml_exact_relative_slack, label="ML Ideal", marker='*', markevery=20)
+        # ax2.plot(data_ml_conserv_relative_slack, p_ml_conserv_relative_slacke, label="ML Conserv.", marker=mrk.TICKRIGHT, markevery=20)
+
+        # ax1.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+        ax1.legend()
+
+        ax2.set_xlabel('Relative Slack')
+        ax2.set_ylabel('')
+        plt.tight_layout()
+        fig.show()
+        filename = self.prefix_ap[:-3] + "plot.pdf"
+        fig.savefig(filename)
--- main.py
+++ main.py
@@ -1,101 +1,35 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import matplotlib.pyplot as plt
-import matplotlib.markers as mrk
-import numpy as np
+
 from append_files import ManageStatistics
 import sys
 
-def run():
-    # dc_relative_slack = np.loadtxt('dc-relative-slack.txt')
-    # static_relative_slack = np.loadtxt('static-relative-slack.txt')
-    # dc_absolute_slack = np.loadtxt('dc-absolute-slack.txt')
-    # static_absolute_slack = np.loadtxt('static-absolute-slack.txt')
-
-    dc_relative_slack = np.loadtxt('cloudlab/slacks/ml-based-comp/dc-relative-slack.txt')
-    static_relative_slack = np.loadtxt('cloudlab/slacks/ml-based-comp/static-relative-slack.txt')
-    dc_absolute_slack = np.loadtxt('cloudlab/slacks/ml-based-comp/dc-absolute-slack.txt')
-    static_absolute_slack = np.loadtxt('cloudlab/slacks/ml-based-comp/static-absolute-slack.txt')
-
-    ml_exact_relative_slack = np.loadtxt('cloudlab/slacks/ml-based-comp/ml-exact-relative-slack.txt')
-    ml_conserv_relative_slack = np.loadtxt('cloudlab/slacks/ml-based-comp/ml-conserv-relative-slack.txt')
-    ml_exact_abs_slack = np.loadtxt('cloudlab/slacks/ml-based-comp/ml-exact-absolute-slack.txt')
-    ml_conserv_abs_slack = np.loadtxt('cloudlab/slacks/ml-based-comp/ml-conserv-absolute-slack.txt')
-    # print(dc_relative_slack)
-    # create some randomly ddistributed data:
-
-    # sort the data:
-    data_sorted_dc_relative = np.sort(dc_relative_slack)# / 1000 #convert to ms
-    data_sorted_static_relative = np.sort(static_relative_slack)
-    data_sorted_dc_absolute = np.sort(dc_absolute_slack) #/ 100 #convert to cores
-    data_sorted_static_absolute = np.sort(static_absolute_slack) #/ 100 #convert to cores
-
-    data_ml_exact_relative_slack = np.sort(ml_exact_relative_slack)# / 1000 #convert to ms
-    data_ml_conserv_relative_slack = np.sort(ml_conserv_relative_slack)
-    data_ml_exact_abs_slack = np.sort(ml_exact_abs_slack) #/ 100 #convert to cores
-    data_ml_conserv_abs_slack = np.sort(ml_conserv_abs_slack) #/ 100 #convert to cores
-
-    # print(data_sorted_dc_relative)
-    # print(data_sorted_static_relative)
-
-    # calculate the proportional values of samples
-    p_dc_relative = 1. * np.arange(len(dc_relative_slack)) / (len(dc_relative_slack) - 1)
-    p_static_relative = 1. * np.arange(len(static_relative_slack)) / (len(static_relative_slack) - 1)
-    p_dc_absolute = 1. * np.arange(len(dc_absolute_slack)) / (len(dc_absolute_slack) - 1)
-    p_static_absolute = 1. * np.arange(len(static_absolute_slack)) / (len(static_absolute_slack) - 1)
-
-    p_ml_exact_relative_slack = 1. * np.arange(len(data_ml_exact_relative_slack)) / (len(data_ml_exact_relative_slack) - 1)
-    p_ml_conserv_relative_slacke = 1. * np.arange(len(data_ml_conserv_relative_slack)) / (len(data_ml_conserv_relative_slack) - 1)
-    p_ml_exact_abs_slack = 1. * np.arange(len(data_ml_exact_abs_slack)) / (len(data_ml_exact_abs_slack) - 1)
-    p_ml_conserv_abs_slack = 1. * np.arange(len(data_ml_conserv_abs_slack)) / (len(data_ml_conserv_abs_slack) - 1)
-
-    # plot the sorted data:
-    fig = plt.figure()
-    # fig, axs = plt.subplot(1, 2)
-    # axs[0,0]
-
-    ax1 = fig.add_subplot(211)
-    ax1.plot(data_sorted_dc_absolute, p_dc_absolute, label="DC", marker='+', markevery=20)
-    ax1.plot(data_sorted_static_absolute, p_static_absolute, label="Static", marker='x', markevery=20)
-    ax1.plot(data_ml_exact_abs_slack, p_ml_exact_abs_slack, label="ML Ideal", marker='*', markevery=20)
-    ax1.plot(data_ml_conserv_abs_slack, p_ml_conserv_abs_slack, label="ML Conserv.", marker=mrk.TICKRIGHT, markevery=20)
-
-    ax1.set_xlabel('Absolute Slack')
-    ax1.set_ylabel('')
-
-    ax2 = fig.add_subplot(212)
-    ax2.plot(data_sorted_dc_relative, p_dc_relative, label="DC", marker='+', markevery=20)
-    ax2.plot(data_sorted_static_relative, p_static_relative, label="Static", marker='x', markevery=20)
-    ax2.plot(data_ml_exact_relative_slack, p_ml_exact_relative_slack, label="ML Ideal", marker='*', markevery=20)
-    ax2.plot(data_ml_conserv_relative_slack, p_ml_conserv_relative_slacke, label="ML Conserv.", marker=mrk.TICKRIGHT, markevery=20)
-
-
-    # ax1.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
-    ax1.legend()
-
-    ax2.set_xlabel('Relative Slack')
-    ax2.set_ylabel('')
-    plt.tight_layout()
-    fig.show()
-    fig.savefig('ml-absolute-and-relative-slack.pdf')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    if len(sys.argv) != 7:
+    if len(sys.argv) != 4:
         print("ERROR: Need type, ratio, num_files args")
         sys.exit(-1)
     measurement = sys.argv[1]       # slack/throttles/ooms
     resource = sys.argv[2]          # cpu/mem
-    ratio = sys.argv[3]             # 0.5x, 1x, 1.5x, etc
-    duration = sys.argv[4]          # 1 min, 2 min, etc
-    system = sys.argv[5]            # DC, K8s, autopilot
-    num_files = int(sys.argv[6])    # files to read in
+    load_type = sys.argv[3]         # static, exp, burst
 
-    manageStats = ManageStatistics(measurement, resource, ratio, duration, system)
-    agg_file_folder = manageStats.aggregate_per_period_to_per_second()
-    manageStats.aggregate_into_one_file(agg_file_folder)
+    manageStats = ManageStatistics(measurement, resource, load_type)
 
-    # run(filename_prefix, ratio)
-    # run()
+    """ DC STUFF """
+    manageStats.aggregate_per_period_to_per_second()
+    manageStats.aggregate_into_one_file()
+
+    """ AUTOPILOT STUFF """
+    manageStats.remove_low_usage_containers()
+    manageStats.aggregate_into_one_file_autopilot()
+
+    """ PLOT """
+    manageStats.run()
+
+    # agg_file_folder = manageStats.aggregate_per_period_to_per_second_autopilot()
+    # manageStats.aggregate_into_one_file_autopilot(agg_file_folder)
+
+    # run(measurement, resource, load_type, duration, system)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
