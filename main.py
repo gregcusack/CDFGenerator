742b74866180d1d248d66f20134c3fda3cae88d0
greg <gregorycusack@gmail.com>
2022-01-21 16:34:38 -0700

    update

--- main.py
+++ main.py
@@ -3,78 +3,36 @@
 
 from append_files import ManageStatistics
 import sys
+import matplotlib.pyplot as plt
+import matplotlib.markers as mrk
+import numpy as np
+np.set_printoptions(threshold=sys.maxsize)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    if len(sys.argv) <= 5:
-        print("ERROR: Need type, ratio, num_files args")
-        sys.exit(-1)
-    service = sys.argv[1]
-    measurement = sys.argv[2]       # slack/throttles/ooms
-    resource = sys.argv[3]          # cpu/mem
-    load_type = sys.argv[4]         # static, exp, burst
-    rel_slack = sys.argv[5]
-    multiplier = 0
-    if len(sys.argv) == 7:
-        try:
-            multiplier = int(sys.argv[6])
-        except:
-            multiplier = float(sys.argv[6])
+    cold_start_file = "/home/greg/kernel/Distributed-Containers/testing/cold-starts/cold-starts.txt"
+
+    cold_starts = np.loadtxt(cold_start_file)
+
+    print("Mean: " + str(cold_starts.mean()))
+    print("Median: " + str(np.median(cold_starts)))
+    print("Stddev: " + str(cold_starts.std()))
+    print("Variance: " + str(cold_starts.var()))
 
 
-    manageStats = ManageStatistics(service, measurement, resource, load_type, multiplier, rel_slack)
+
+    cold_starts_sorted = np.sort(cold_starts)
+
+    p_cold_starts = 1. * np.arange(len(cold_starts_sorted)) / (len(cold_starts_sorted) - 1)
+
+    # fig, ax = plt.subplots(figsize=(5, 3))
+    fig = plt.figure(figsize=(6, 3))
+    ax1 = fig.add_subplot(111)
+    ax1.plot(cold_starts_sorted, p_cold_starts, marker='+', markevery=20)
 
 
-    if multiplier == -1: #plot everything on same graph
-        manageStats.run_all()
-
-    elif multiplier != 0:
-        if resource == "cpu":
-            """ DC STUFF """
-            manageStats.aggregate_per_period_to_per_second()
-            manageStats.aggregate_into_one_file()
-
-            """ STATIC """
-            manageStats.remove_low_usage_containers_static()
-            manageStats.aggregate_into_one_file_static()
-
-
-        elif resource == "mem":
-            manageStats.remove_low_usage_containers("dc")
-            manageStats.aggregate_into_one_file_autopilot("dc")
-
-            print("################################## DC Stuff DONE #################################")
-
-
-            manageStats.remove_low_usage_containers("static")
-            manageStats.aggregate_into_one_file_autopilot("static")
-
-        manageStats.run_static()
-
-    else:
-        if resource == "cpu":
-            """ DC STUFF """
-            manageStats.aggregate_per_period_to_per_second()
-            manageStats.aggregate_into_one_file()
-
-            """ AUTOPILOT STUFF """
-            manageStats.remove_low_usage_containers("ap")
-            manageStats.aggregate_into_one_file_autopilot("ap")
-
-        elif resource == "mem":
-            manageStats.remove_low_usage_containers("dc")
-            manageStats.aggregate_into_one_file_autopilot("dc")
-
-            manageStats.remove_low_usage_containers("ap")
-            manageStats.aggregate_into_one_file_autopilot("ap")
-
-        """ PLOT """
-        manageStats.run()
-
-    # agg_file_folder = manageStats.aggregate_per_period_to_per_second_autopilot()
-    # manageStats.aggregate_into_one_file_autopilot(agg_file_folder)
-
-    # run(measurement, resource, load_type, duration, system)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    ax1.set_xlabel('Per-Container Connection to Controller Overhead (ms)')
+    ax1.set_ylabel('')
+    plt.tight_layout()
+    fig.show()
